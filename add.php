a118913a2cde9a3a5cf7858c4053a47242fa1675
laurentnunez <lnunez@hotmail.fr>
2023-04-12 09:12:12 +0200

    connexion and add expense form

---
+++ add.php
@@ -0,0 +1,14 @@
+<div class="add-container">
+    <form>
+      <input type="text" placeholder="ajouter une dépense" method="post" name="exepense" class="form control" required=required>
+      <input type="mount" placeholder="montant" name="mount" method="post" class="form control" required=required>
+      <select name="category">
+        <option value="restaurant">Restaurant</option>
+        <option value="transport">Transport</option>
+        <option value="courses">Courses</option>
+        <option value="santé">Santé</option>
+        <option value="cadeaux">Cadeaux</option>
+      </select>
+      <button type="submit">Connexion</button>
+    </form>
+</div>
---
+++ connexion.php
@@ -0,0 +1,7 @@
+<div class="connexion-container">
+    <form>
+      <input type="text" placeholder="utilisateur" method="post" name="username" class="form control" required=required>
+      <input type="password" placeholder="mot de passe" name="password" method="post" class="form control" required=required>
+      <button type="submit">Connexion</button>
+    </form>
+</div>
